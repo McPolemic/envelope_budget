0386ee3f65f6a1a1dfa2f5192c09fb7c582db17b
Adam Lukens <adam@testdouble.com>
2017-09-22 18:30:35 -0400

    Pass logger into app/lib code

--- app/lib/messenger.rb
+++ app/lib/messenger.rb
@@ -1,11 +1,11 @@
 class Messenger
-  def self.send_message(number, message, from_number: ENV.fetch('PHONE_NUMBER'))
+  def self.send_message(number, message, from_number: ENV.fetch('PHONE_NUMBER'), logger: nil)
     account_sid = ENV.fetch("TWILIO_ACCOUNT_SID")
     auth_token = ENV.fetch("TWILIO_AUTH_TOKEN")
 
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    logger.info %("Sending message \"#{message}\" to #{from_number}")
+    logger.info %("Sending message \"#{message}\" to #{from_number}") unless logger.nil?
     message = client.messages.create(
       body: message,
       to: number,
