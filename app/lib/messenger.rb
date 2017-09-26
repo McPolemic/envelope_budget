55615dc573d78708d0451671c7adbf5594edef14
Adam Lukens <adam@testdouble.com>
2017-09-26 10:11:29 -0400

    Log that we're sending a message to the right number

--- app/lib/messenger.rb
+++ app/lib/messenger.rb
@@ -5,7 +5,7 @@ class Messenger
 
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    logger.info %("Sending message \"#{message}\" to #{from_number}") unless logger.nil?
+    logger.info %("Sending message \"#{message}\" to #{number}") unless logger.nil?
     message = client.messages.create(
       body: message,
       to: number,
