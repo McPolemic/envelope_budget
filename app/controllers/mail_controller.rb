71a8282900690c09369249ead6d490762af28faf
Adam Lukens <adam@testdouble.com>
2019-04-12 12:54:05 -0400

    Email - Log more useful information

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,6 +1,9 @@
 class MailController < ApplicationController
   def create
-    logger.info "Email received: #{params}"
+    logger.info "Email received: #{params[:subject]}"
+    logger.info "Email received to: #{params[:to]"}"
+    logger.info "Email received from: #{params[:from]"}"
+    logger.info "Email body: #{params[:text]"}"
 
     head :ok
   end
